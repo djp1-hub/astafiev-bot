897402f7ba5ad812eae37c4b4d6ee71fbba32947
gennady <djp1@yandex.ru>
2024-06-25 14:32:19 +0200

    add voice module

--- src/voice_handler.py
+++ src/voice_handler.py
@@ -2,7 +2,7 @@ import openai
 from config import OPENAI_API_KEY
 from telegram import Update
 from telegram.ext import CallbackContext
-import soundfile as sf
+from pydub import AudioSegment
 import io
 
 openai.api_key = OPENAI_API_KEY
@@ -17,14 +17,14 @@ class VoiceHandler:
         file_byte_array = voice_file.download_as_bytearray()
 
         # Преобразование ogg в wav в памяти
-        input_audio = io.BytesIO(file_byte_array)
-        data, samplerate = sf.read(input_audio, format='ogg')
-        output_audio = io.BytesIO()
-        sf.write(output_audio, data, samplerate, format='wav')
-        output_audio.seek(0)
+        ogg_audio = AudioSegment.from_ogg(io.BytesIO(file_byte_array))
+        wav_io = io.BytesIO()
+        ogg_audio.export(wav_io, format="wav")
+        wav_io.seek(0)
+        wav_io.name = "voice_message.wav"
 
         # Чтение файла в бинарном режиме
-        transcript = openai.Audio.transcribe("whisper-1", output_audio)
+        transcript = openai.Audio.transcribe("whisper-1", wav_io)
 
         # Отправка текста пользователю
         update.message.reply_text(transcript["text"])
